06dbb2e2043b3ee2e8db2d0c3ba99b6714932c00
didier <didier-defrance@wanadoo.fr>
2017-11-20 10:44:58 +0100

    version initiale

--- init_push.bat
+++ init_push.bat
@@ -2,8 +2,8 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOriginAfcepfServerSpringMvc https://didier-tp:pwd007!@github.com/didier-tp/afcepf_serverSpringMvc.git
-git push -u gitHubOriginAfcepfServerSpringMvc master
+git remote add gitHubOriginAfcepfServerSpringMvc2 https://didier-tp:pwd007!@github.com/didier-tp/afcepf_serverSpringMvc.git
+git push -u gitHubOriginAfcepfServerSpringMvc2 master
 pause
 
 REM open with text editor
